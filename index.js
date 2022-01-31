7138f8d87b926c6f10938dd8c2977221a4a0b99e
the-Awakening-of-god <74119951+the-Awakening-of-god@users.noreply.github.com>
2022-01-31 12:20:57 -0600

    Update index.js
    Test to see if it will work

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const e = require('express');
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-
-app.use(express.static("Public"));
-
-
-app.get ('/', (req, res) => {
-    res.sendFile(__dirname + "/public/sfx.html")
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-
 app.listen(port, () => {
-    console.log(`SFX app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}`)
 })
