5942fe0c7c2558a8dfbc44ecb19abafe62e50c24
the-Awakening-of-god <74119951+the-Awakening-of-god@users.noreply.github.com>
2022-02-01 13:45:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const port = 3000;
 
 
 app.use(express.static(__dirname + '/Public/'));
-
- 
+app.use(function (req, res, next) {
+  res.set('Cache-control', 'public, max-age=7200')
+}) 
 app.get ('/', (req, res) => {
     res.sendFile('/Public/sfx.html', { root: __dirname});
 })
