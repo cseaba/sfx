158c591b8d05de427edcf0adb5c877bead3e0bb7
the-Awakening-of-god <74119951+the-Awakening-of-god@users.noreply.github.com>
2022-02-01 14:01:40 -0600

    Update index.js
    maybe allows for caching

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ const port = 3000;
 
 app.use(express.static(__dirname + '/Public/'));
 
+let setCache = function (req, res, next) {
+  // here you can define period in second, this one is 5 minutes
+  const period = 60 * 5 
+
+  // you only want to cache for GET requests
+  if (req.method == 'GET') {
+    res.set('Cache-control', `public, max-age=${period}`)
+  } else {
+    // for the other requests set strict no caching parameters
+    res.set('Cache-control', `no-store`)
+  }
+
+  // remember to call next() to pass on the request
+  next()
+}
+
+// now call the new middleware function in your app
+
+app.use(setCache);
+
 app.get ('/', (req, res) => {
     res.sendFile('/Public/sfx.html', { root: __dirname});
 })
