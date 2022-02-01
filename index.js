64f3ac415593af88baffc86783029ad64a0bddfd
Garrett Seaba <calebseaba@gmail.com>
2022-01-31 21:24:14 -0600

    idk know anymore

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const e = require('express');
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
 
-app.use(express.static('Public', { root: __dirname}));
+app.use(express.static('/Public', {root: __dirname}));
 
-
+ 
 app.get ('/', (req, res) => {
     res.sendFile('/Public/sfx.html', { root: __dirname});
 })
